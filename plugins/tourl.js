96e82048ea3bf45bf3cc2b165c19f8f913a0babf
Sniperghost662009 <125476080+Sniperghost662009@users.noreply.github.com>
2024-04-28 21:25:17 -0600

    Create tourl.js

---
+++ plugins/tourl.js
@@ -0,0 +1,15 @@
+import uploadFile from '../lib/uploadFile.js';
+import uploadImage from '../lib/uploadImage.js';
+const handler = async (m) => {
+  const q = m.quoted ? m.quoted : m;
+  const mime = (q.msg || q).mimetype || '';
+  if (!mime) throw '*ʀᴇꜱᴘᴏɴᴅᴇ ᴀ ᴜɴᴀ ɪᴍᴀɢᴇɴ ᴏ ᴠɪᴅᴇᴏ ᴇʟ ᴄᴜᴀʟ ꜱᴇʀᴀ ᴄᴏɴᴠᴇʀᴛɪᴅᴏ ᴀ ᴇɴʟᴀᴄᴇ*';
+  const media = await q.download();
+  const isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
+  const link = await (isTele ? uploadImage : uploadFile)(media);
+  m.reply(`* ᴇɴʟᴀᴄᴇ ᴀ ꜱᴜ ᴀʀᴄʜɪᴠᴏ:* ${link}`);
+};
+handler.help = ['tourl <reply image>'];
+handler.tags = ['tools'];
+handler.command = /^(upload|tourl)$/i;
+export default handler;
