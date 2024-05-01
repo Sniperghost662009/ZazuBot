10b2f60cf26907c22ff853fc2a9c906943ab9793
Sniperghost662009 <125476080+Sniperghost662009@users.noreply.github.com>
2024-05-01 16:08:04 -0600

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -1,218 +1,122 @@
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
-import * as fs from 'fs'
-import * as path from 'path'
-import * as  crypto from 'crypto'
-import { ffmpeg } from './converter.js'
-import fluent_ffmpeg from 'fluent-ffmpeg'
-import { spawn } from 'child_process'
-import uploadFile from './uploadFile.js'
-import uploadImage from './uploadImage.js'
-import { fileTypeFromBuffer } from 'file-type'
-import webp from 'node-webpmux'
-import fetch from 'node-fetch'
+import {sticker} from '../lib/sticker.js';
+import uploadFile from '../lib/uploadFile.js';
+import uploadImage from '../lib/uploadImage.js';
+import {webp2png} from '../lib/webp2mp4.js';
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const tmp = path.join(__dirname, '../tmp')
-/**
- * Image to Sticker
- * @param {Buffer} img Image Buffer
- * @param {String} url Image URL
- */
-function sticker2(img, url) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      if (url) {
-        const res = await fetch(url)
-        if (res.status !== 200) throw await res.text()
-        img = await res.buffer()
-      }
-      const inp = path.join(tmp, +new Date + '.jpeg')
-      await fs.promises.writeFile(inp, img)
-      const ff = spawn('ffmpeg', [
-        '-y',
-        '-i', inp,
-        '-vf', 'scale=512:512:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=512:512:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1',
-        '-f', 'png',
-        '-'
-      ])
-      ff.on('error', reject)
-      ff.on('close', async () => {
-        await fs.promises.unlink(inp)
-      })
-      const bufs = []
-      const [_spawnprocess, ..._spawnargs] = [...(module.exports.support.gm ? ['gm'] : module.exports.magick ? ['magick'] : []), 'convert', 'png:-', 'webp:-']
-      const im = spawn(_spawnprocess, _spawnargs)
-      im.on('error', e => conn.reply(m.chat, util.format(e), m))
-      im.stdout.on('data', chunk => bufs.push(chunk))
-      ff.stdout.pipe(im.stdin)
-      im.on('exit', () => {
-        resolve(Buffer.concat(bufs))
-      })
-    } catch (e) {
-      reject(e)
-    }
-  })
-}
-
-/**
- * Image/Video to Sticker
- * @param {Buffer} img Image/Video Buffer
- * @param {String} url Image/Video URL
- * @param {String} packname EXIF Packname
- * @param {String} author EXIF Author
- */
-async function sticker3(img, url, packname, author) {
-  url = url ? url : await uploadFile(img)
-  const res = await fetch('https://chat.whatsapp.com/D98qLrpbfNa3B8meS3NoWwrffffffffff4ppppp' + new URLSearchParams(Object.entries({
-    url,
-    packname,
-    author
-  })))
-  return await res.buffer()
-}
-
-/**
- * Image to Sticker
- * @param {Buffer} img Image/Video Buffer
- * @param {String} url Image/Video URL
- */
-async function sticker4(img, url) {
-  if (url) {
-    const res = await fetch(url)
-    if (res.status !== 200) throw await res.text()
-    img = await res.buffer()
-  }
-  return await ffmpeg(img, [
-    '-vf', 'scale=512:512:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=512:512:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1'
-  ], 'jpeg', 'webp')
-}
-
-async function sticker5(img, url, packname, author, categories = [''], extra = {}) {
-  const { Sticker } = await import('wa-sticker-formatter')
-  const stickerMetadata = {
-    type: 'default',
-    pack: packname,
-    author,
-    categories,
-    ...extra
-  }
-  return (new Sticker(img ? img : url, stickerMetadata)).toBuffer()
-}
-
-/**
- * Convert using fluent-ffmpeg
- * @param {string} img 
- * @param {string} url 
- */
-function sticker6(img, url) {
-  return new Promise(async (resolve, reject) => {
-    if (url) {
-      const res = await fetch(url)
-      if (res.status !== 200) throw await res.text()
-      img = await res.buffer()
-    }
-    const type = await fileTypeFromBuffer(img) || {
-      mime: 'application/octet-stream',
-      ext: 'bin'
-    }
-    if (type.ext == 'bin') reject(img)
-    const tmp = path.join(__dirname, `../tmp/${+ new Date()}.${type.ext}`)
-    const out = path.join(tmp + '.webp')
-    await fs.promises.writeFile(tmp, img)
-    // https://github.com/MhankBarBar/termux-wabot/blob/main/index.js#L313#L368
-    const Fffmpeg = /video/i.test(type.mime) ? fluent_ffmpeg(tmp).inputFormat(type.ext) : fluent_ffmpeg(tmp).input(tmp)
-    Fffmpeg
-      .on('error', function (err) {
-        console.error(err)
-        fs.promises.unlink(tmp)
-        reject(img)
-      })
-      .on('end', async function () {
-        fs.promises.unlink(tmp)
-        resolve(await fs.promises.readFile(out))
-      })
-      .addOutputOptions([
-        `-vcodec`, `libwebp`, `-vf`,
-        `scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`
-      ])
-      .toFormat('webp')
-      .save(out)
-  })
-}
-/**
- * Add WhatsApp JSON Exif Metadata
- * Taken from https://github.com/pedroslopez/whatsapp-web.js/pull/527/files
- * @param {Buffer} webpSticker 
- * @param {String} packname 
- * @param {String} author 
- * @param {String} categories 
- * @param {Object} extra 
- * @returns 
- */
-async function addExif(webpSticker, packname, author, categories = [''], extra = {}) {
-  const img = new webp.Image();
-  const stickerPackId = crypto.randomBytes(32).toString('hex');
-  const json = { 'sticker-pack-id': stickerPackId, 'sticker-pack-name': packname, 'sticker-pack-publisher': author, 'emojis': categories, ...extra };
-  const exifAttr = Buffer.from([0x49, 0x49, 0x2A, 0x00, 0x08, 0x00, 0x00, 0x00, 0x01, 0x00, 0x41, 0x57, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0x00, 0x00, 0x00]);
-  const jsonBuffer = Buffer.from(JSON.stringify(json), 'utf8');
-  const exif = Buffer.concat([exifAttr, jsonBuffer]);
-  exif.writeUIntLE(jsonBuffer.length, 14, 4);
-  await img.load(webpSticker)
-  img.exif = exif
-  return await img.save(null)
-}
-
-/**
- * Image/Video to Sticker
- * @param {Buffer} img Image/Video Buffer
- * @param {String} url Image/Video URL
- * @param {...String} 
-*/
-async function sticker(img, url, ...args) {
-  let lastError, stiker
-  for (const func of [
-    sticker3, global.support.ffmpeg && sticker6, sticker5,
-    global.support.ffmpeg && global.support.ffmpegWebp && sticker4,
-    global.support.ffmpeg && (global.support.convert || global.support.magick || global.support.gm) && sticker2,
-  ].filter(f => f)) {
-    try {
-      stiker = await func(img, url, ...args)
-      if (stiker.includes('html')) continue
-      if (stiker.includes('WEBP')) {
-        try {
-          return await addExif(stiker, ...args)
-        } catch (e) {
-          console.error(e)
-          return stiker
+const handler = async (m, {conn, args, usedPrefix, command}) => {
+  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+  let stiker = false;
+  const user = db.data.users[m.sender];
+  try {
+    const q = m.quoted ? m.quoted : m;
+    const mime = (q.msg || q).mimetype || q.mediaType || '';
+    if (/webp|image|video/g.test(mime)) {
+      const img = await q.download?.();
+      if (!img) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴 𝙰 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾, 𝙸𝙼𝙰𝙶𝙴𝙽 𝙾 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝚄𝙽𝙰 𝙸𝙼𝙰𝙶𝙴𝙽 𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝙲𝙸𝙾́𝙽 .𝚓𝚙𝚐 𝙴𝙻 𝙲𝚄𝙰𝙻 𝚂𝙴𝚁𝙰 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝙳𝙾 𝙴𝙽 𝚂𝚃𝙸𝙲𝙺𝙴𝚁, 𝙳𝙴𝙱𝙴 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴𝚁 𝙾 𝚄𝚂𝙰𝚁 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command}*`;
+      let out;
+      try {
+        stiker = await sticker(img, false, global.packname, global.author);
+      } catch (e) {
+        console.error(e);
+      } finally {
+        if (!stiker) {
+          if (/webp/g.test(mime)) out = await webp2png(img);
+          else if (/image/g.test(mime)) out = await uploadImage(img);
+          else if (/video/g.test(mime)) out = await uploadFile(img);
+          if (typeof out !== 'string') out = await uploadImage(img);
+          stiker = await sticker(false, out, global.packname, global.author);
         }
       }
-      throw stiker.toString()
-    } catch (err) {
-      lastError = err
-      continue
+    } else if (args[0]) {
+      if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author);
+      else return m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 / 𝚄𝚁𝙻 / 𝙻𝙸𝙽𝙺 𝙽𝙾 𝙴𝚂 𝚅𝙰𝙻𝙸𝙳𝙰, 𝙻𝙰 𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝙲𝙸𝙾𝙽 𝙳𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 / 𝚄𝚁𝙻 / 𝙻𝙸𝙽𝙺 𝙳𝙴𝙱𝙴 𝚂𝙴𝚁 .𝚓𝚙𝚐, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix}s https://telegra.ph/file/0dc687c61410765e98de2.jpg*');
     }
+  } catch (e) {
+    console.error(e);
+    if (!stiker) stiker = e;
+  } finally {
+    if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m);
+    else throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙾𝙲𝚄𝚁𝚁𝙸𝙾 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁, 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾. 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴 𝙰 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾, 𝙸𝙼𝙰𝙶𝙴𝙽 𝙾 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝚄𝙽𝙰 𝙸𝙼𝙰𝙶𝙴𝙽 𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝙲𝙸𝙾́𝙽 .𝚓𝚙𝚐 𝙴𝙻 𝙲𝚄𝙰𝙻 𝚂𝙴𝚁𝙰 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝙳𝙾 𝙴𝙽 𝚂𝚃𝙸𝙲𝙺𝙴𝚁*';
   }
-  console.error(lastError)
-  return lastError
-}
+};
+handler.help = ['sfull'];
+handler.tags = ['sticker'];
+handler.command = /^s(tic?ker)?(gif)?(wm)?$/i;
 
-const support = {
-  ffmpeg: true,
-  ffprobe: true,
-  ffmpegWebp: true,
-  convert: true,
-  magick: false,
-  gm: false,
-  find: false
-}
+export default handler;
 
-export {
-  sticker,
-  sticker2,
-  sticker3,
-  sticker4,
-  sticker6,
-  addExif,
-  support
+const isUrl = (text) => {
+  return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'));
+};
+
+
+/* import fetch from 'node-fetch'
+import { addExif } from '../lib/sticker.js'
+import { sticker } from '../lib/sticker.js'
+import uploadFile from '../lib/uploadFile.js'
+import uploadImage from '../lib/uploadImage.js'
+import { webp2png } from '../lib/webp2mp4.js'
+import { Sticker } from 'wa-sticker-formatter'
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  let stiker = false
+  try {
+    let [packname, ...author] = args.join` `.split`|`
+    author = (author || []).join`|`
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || q.mediaType || ''
+    if (/webp/g.test(mime)) {
+      let img = await q.download?.()
+      stiker = await addExif(img, packname || global.packname, author || global.author)
+    } else if (/image/g.test(mime)) {
+      let img = await q.download?.()
+      stiker = await createSticker(img, false, packname || global.packname, author || global.author)
+    } else if (/video/g.test(mime)) {
+      let img = await q.download?.()
+      stiker = await mp4ToWebp(img, { pack: packname || global.packname, author: author || global.author })
+    } else if (args[0] && isUrl(args[0])) {
+      stiker = await createstiker(false, args[0], '', author, 20)
+    } else throw `*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴 𝙰 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾, 𝙸𝙼𝙰𝙶𝙴𝙽 𝙾 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝚄𝙽𝙰 𝙸𝙼𝙰𝙶𝙴𝙽 𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝙲𝙸𝙾́𝙽 .𝚓𝚙𝚐 𝙴𝙻 𝙲𝚄𝙰𝙻 𝚂𝙴𝚁𝙰 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝙳𝙾 𝙴𝙽 𝚂𝚃𝙸𝙲𝙺𝙴𝚁, 𝙳𝙴𝙱𝙴 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴𝚁 𝙾 𝚄𝚂𝙰𝚁 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command}*`
+  } catch (error) {
+    console.log(error)
+    try {
+      let [packname, ...author] = args.join` `.split`|`
+      author = (author || []).join`|`
+      let q = m.quoted ? m.quoted : m
+      let mime = (q.msg || q).mimetype || q.mediaType || ''
+      let img = await q.download?.()
+      if (/webp/g.test(mime)) out = await webp2png(img)
+      else if (/image/g.test(mime)) out = await uploadImage(img)
+      else if (/video/g.test(mime)) out = await uploadFile(img)
+      if (typeof out !== 'string') out = await uploadImage(img)
+      stiker = await stiker(false, out, global.packname, global.author)
+      if (args[0] && isUrl(args[0])) {
+        stiker = await stiker(false, args[0], global.packname, global.author)
+      } else {
+        throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 / 𝚄𝚁𝙻 / 𝙻𝙸𝙽𝙺 𝙽𝙾 𝙴𝚂 𝚅𝙰𝙻𝙸𝙳𝙰, 𝙻𝙰 𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝙲𝙸𝙾𝙽 𝙳𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 / 𝚄𝚁𝙻 / 𝙻𝙸𝙽𝙺 𝙳𝙴𝙱𝙴 𝚂𝙴𝚁 .𝚓𝚙𝚐, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix}s https://telegra.ph/file/0dc687c61410765e98de2.jpg*`
+      }
+    } catch (error) {
+      stiker = `*[❗𝐈𝐍𝐅𝐎❗] 𝙾𝙲𝚄𝚁𝚁𝙸𝙾 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁, 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾. 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴 𝙰 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾, 𝙸𝙼𝙰𝙶𝙴𝙽 𝙾 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝚄𝙽𝙰 𝙸𝙼𝙰𝙶𝙴𝙽 𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝙲𝙸𝙾́𝙽 .𝚓𝚙𝚐 𝙴𝙻 𝙲𝚄𝙰𝙻 𝚂𝙴𝚁𝙰 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝙳𝙾 𝙴𝙽 𝚂𝚃𝙸𝙲𝙺𝙴𝚁`
+    }
+  } finally {
+    m.reply(stiker)
+  }
 }
+handler.help = ['sfull']
+handler.tags = ['sticker']
+handler.command = /^s(tic?ker)?(gif)?(wm)?$/i
+export default handler
+const isUrl = (text) => text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))
+async function createSticker(img, url, packName, authorName, quality) {
+let stickerMetadata = { type: 'full', pack: packName, author: authorName, quality }
+return (new Sticker(img ? img : url, stickerMetadata)).toBuffer()}
+async function mp4ToWebp(file, stickerMetadata) {
+if (stickerMetadata) {
+if (!stickerMetadata.pack) stickerMetadata.pack = '‎'
+if (!stickerMetadata.author) stickerMetadata.author = '‎'
+if (!stickerMetadata.crop) stickerMetadata.crop = false
+} else if (!stickerMetadata) { stickerMetadata = { pack: '‎', author: '‎', crop: false }}
+let getBase64 = file.toString('base64')
+const Format = { file: `data:video/mp4;base64,${getBase64}`, processOptions: { crop: stickerMetadata?.crop, startTime: '00:00:00.0', endTime: '00:00:7.0', loop: 0 }, stickerMetadata: { ...stickerMetadata }, sessionInfo: { WA_VERSION: '2.2106.5', PAGE_UA: 'WhatsApp/2.2037.6 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36', WA_AUTOMATE_VERSION: '3.6.10 UPDATE AVAILABLE: 3.6.11', BROWSER_VERSION: 'HeadlessChrome/88.0.4324.190', OS: 'Windows Server 2016', START_TS: 1614310326309, NUM: '6247', LAUNCH_TIME_MS: 7934, PHONE_VERSION: '2.20.205.16' }, config: { sessionId: 'session', headless: true, qrTimeout: 20, authTimeout: 0, cacheEnabled: false, useChrome: true, killProcessOnBrowserClose: true, throwErrorOnTosBlock: false, chromiumArgs: ['--no-sandbox', '--disable-setuid-sandbox', '--aggressive-cache-discard', '--disable-cache', '--disable-application-cache', '--disable-offline-load-stale-cache', '--disk-cache-size=0'], executablePath: 'C:\\\\Program Files (x86)\\\\Google\\\\Chrome\\\\Application\\\\chrome.exe', skipBrokenMethodsCheck: true, stickerServerEndpoint: true }}
+let res = await fetch('https://sticker-api.openwa.dev/convertMp4BufferToWebpDataUrl', { method: 'post', headers: { Accept: 'application/json, text/plain, /', 'Content-Type': 'application/json;charset=utf-8', }, body: JSON.stringify(Format)})
+return Buffer.from((await res.text()).split(';base64,')[1], 'base64')}*/
