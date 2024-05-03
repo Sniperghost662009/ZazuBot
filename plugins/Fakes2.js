5498c78ec6588e25c7117eb938a44bfa219a42da
Sniperghost662009 <125476080+Sniperghost662009@users.noreply.github.com>
2024-05-03 17:03:33 -0600

    Add files via upload

---
+++ plugins/Fakes2.js
@@ -0,0 +1,89 @@
+export async function before(m, { conn }) {
+		// Respuesta de anuncio 
+ global.adReply = {
+			contextInfo: {
+			mentionedJid: [m.sender],
+				forwardingScore: 9999,
+				externalAdReply: {
+				showAdAttribution: true,
+					title: desc,
+					body: desc,
+					mediaUrl: null,
+					description: null,
+					previewType: "PHOTO",
+					thumbnail: catalogo,
+					sourceUrl: channel
+				}
+			}
+		}
+		
+		// Respuesta de descargas de YouTube
+ global.adYT = {
+			contextInfo: {
+			mentionedJid: [m.sender],
+				forwardingScore: 9999,
+				externalAdReply: {
+				showAdAttribution: true,
+					title: desc,
+					body: desc,
+					mediaUrl: null,
+					description: null,
+					previewType: "PHOTO",
+					thumbnail: catalogo,
+					sourceUrl: channel
+				}
+			}
+		}
+		
+		  // Respuesta de Stickers 
+ global.adSticker = {
+			contextInfo: {
+			mentionedJid: [m.sender],
+				forwardingScore: 9999,
+				externalAdReply: {
+				showAdAttribution: true,
+					title: desc,
+					body: desc,
+					mediaUrl: null,
+					description: null,
+					previewType: "PHOTO",
+					thumbnail: catalogo,
+					sourceUrl: channel
+				}
+			}
+		}
+		//Respuesta de busquedas 
+ global.adSearch = {
+			contextInfo: {
+			mentionedJid: [m.sender],
+				forwardingScore: 9999,
+				externalAdReply: {
+				showAdAttribution: true,
+					title: desc,
+					body: desc,
+					mediaUrl: null,
+					description: null,
+					previewType: "PHOTO",
+					thumbnail: catalogo,
+					sourceUrl: channel
+				}
+			}
+		}
+		//Respuesta de Nsfw 
+ global.adNsfw = {
+			contextInfo: {
+			mentionedJid: [m.sender],
+				forwardingScore: 9999,
+				externalAdReply: {
+				showAdAttribution: true,
+					title: desc,
+					body: desc,
+					mediaUrl: null,
+					description: null,
+					previewType: "PHOTO",
+					thumbnail: catalogo,
+					sourceUrl: channel
+				}
+			}
+		}
+}
