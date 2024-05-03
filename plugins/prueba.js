8d4a2fede25aea1701b311eb6d998abfbc467ae8
Sniperghost662009 <125476080+Sniperghost662009@users.noreply.github.com>
2024-05-03 17:35:02 -0600

    Create prueba.js

---
+++ plugins/prueba.js
@@ -0,0 +1,43 @@
+import pkg from '@whiskeysockets/baileys';
+const { generateWAMessageFromContent, proto } = pkg
+
+var handler = async (m, { conn, usedPrefix }) => {
+
+let msg = generateWAMessageFromContent(m.chat, {
+  viewOnceMessage: {
+    message: {
+        "messageContextInfo": {
+          "deviceListMetadata": {},
+          "deviceListMetadataVersion": 2
+        },
+        interactiveMessage: proto.Message.InteractiveMessage.create({
+          body: proto.Message.InteractiveMessage.Body.create({
+            text: "test"
+          }),
+          footer: proto.Message.InteractiveMessage.Footer.create({
+            text: "test"
+          }),
+          header: proto.Message.InteractiveMessage.Header.create({
+            title: "test",
+            subtitle: "test",
+            hasMediaAttachment: false
+          }),
+          nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+            buttons: [
+              {
+                "
+                 "name": "cta_copy",
+                 "buttonParamsJson": "{\"FYTG-9JGH\":\"copy\",\"id\":\"123456789\",\"copy_code\":\"message\"} 
+           ],
+          })
+        })
+    }
+  }
+}, {})
+
+await conn.relayMessage(msg.key.remoteJid, msg.message, { messageId: msg.key.id })
+
+}
+handler.command = /^(mboton)$/i
+
+export default handler  
