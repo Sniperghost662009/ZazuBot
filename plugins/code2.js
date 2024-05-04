9aa8c1a4a421212c19c6f73f9bb4439cea597e6e
Sniperghost662009 <125476080+Sniperghost662009@users.noreply.github.com>
2024-05-03 21:52:10 -0600

    Update code2.js

--- plugins/code2.js
+++ plugins/code2.js
@@ -64,24 +64,15 @@ let handler = async (m, { conn: _conn, args, usedPrefix, command, isOwner }) => 
             defaultQueryTimeoutMs: undefined,
             version
       }
-                async function bbts() {
+async function bbts() {
     let authFolderB = crypto.randomBytes(10).toString('hex').slice(0, 8)
 
-    // Definir phoneNumber antes de methodCode
     let phoneNumber = m.sender.split('@')[0]
-    const methodCode = !!phoneNumber || process.argv.includes("code")
-
-    // Resto del código...
-
-    const methodCodeQR = process.argv.includes("qr")
-    const MethodMobile = process.argv.includes("mobile")
-
-    // Resto del código...
+    const methodCode = !!phoneNumber || process.argv.includes("code") 
+   
 }
 
-        
-
-        let conn = makeWASocket(connectionOptions)
+            let conn = makeWASocket(connectionOptions)
 
         if (methodCode && !conn.authState.creds.registered) {
             if (!phoneNumber) {
