08de02c4783a8d21f2f383a791ee9b2f2a7214ea
Sniperghost662009 <125476080+Sniperghost662009@users.noreply.github.com>
2024-05-03 21:31:13 -0600

    Update serbot-code.js

--- plugins/serbot-code.js
+++ plugins/serbot-code.js
@@ -111,7 +111,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
                                 name: 'cta_copy',
                                 buttonParamsJson: JSON.stringify({
                                     display_text: 'Copiar código',
-                                    copy_code: (`${codeBot}`), // Aquí se debe reemplazar codeBot con tu variable que contiene el código
+                                    copy_code: (`${codeBot}`) // Aquí se debe reemplazar codeBot con tu variable que contiene el código
                                     id: 'copy_button'
                                 })
                             }
